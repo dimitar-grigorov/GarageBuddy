e1c19b544f044413bc9eab2839ae5190d6bc8338
dimitar-grigorov <mitak.backtrack@gmail.com>
2023-08-12 01:11:11 +0300

    Now used theme is build in npm by me. https://www.npmjs.com/package/@grigorov-it/mazer https://github.com/dimitar-grigorov/mazer
    Added to gitignore the themes folder.

---
+++ src/GarageBuddy.Common/Constants/NotificationConstants.cs
@@ -0,0 +1,10 @@
+﻿namespace GarageBuddy.Common.Constants
+{
+    public static class NotificationConstants
+    {
+        public const string NotifyError = "NotifyError";
+        public const string NotifyWarning = "NotifyWarning";
+        public const string NotifyInfo = "NotifyInfo";
+        public const string NotifySuccess = "NotifySuccess";
+    }
+}
