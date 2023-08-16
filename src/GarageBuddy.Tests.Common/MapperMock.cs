9bb49188f1a753bc5e40ecd76429055b762d8041
dimitar-grigorov <mitak.backtrack@gmail.com>
2023-08-16 07:05:45 +0300

    Added Common test project based on NUnit.

---
+++ src/GarageBuddy.Tests.Common/MapperMock.cs
@@ -0,0 +1,33 @@
+﻿namespace GarageBuddy.Tests.Common
+{
+    using System.Reflection;
+
+    using AutoMapper;
+
+    using Services.Data.Models.Vehicle.BrandModel;
+    using Services.Mapping;
+
+    /// <summary>
+    /// This class is a mock of <see cref="IMapper"/>.
+    /// </summary>
+    public static class MapperMock
+    {
+        /// <summary>
+        /// Gets the <see cref="IMapper"/> instance.
+        /// </summary>
+        public static IMapper Instance
+        {
+            get
+            {
+                var assemblies = new Assembly[]
+                {
+                    typeof(BrandModelListServiceModel).GetTypeInfo().Assembly,
+                };
+                AutoMapperConfig.RegisterMappings(assemblies);
+
+                var mapper = AutoMapperConfig.MapperInstance;
+                return mapper;
+            }
+        }
+    }
+}
