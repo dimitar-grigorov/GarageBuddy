af74bae159c72886baf06ba038d2e7b0b9695757
dimitar-grigorov <mitak.backtrack@gmail.com>
2023-08-16 18:56:13 +0300

    Initial creation of Garage entity front infrastructure.

---
+++ src/Services/GarageBuddy.Services.Data.Models/GarageServiceModel.cs
@@ -0,0 +1,28 @@
+﻿namespace GarageBuddy.Services.Data.Models
+{
+    using System.ComponentModel.DataAnnotations;
+
+    using GarageBuddy.Data.Models;
+    using GarageBuddy.Services.Data.Models.Base;
+
+    using Mapping;
+
+    public class GarageServiceModel : BaseListServiceModel, IMapFrom<Garage>, IMapTo<Garage>
+    {
+        public string Name { get; set; } = null!;
+
+        public string? Address { get; set; }
+
+        public string? Email { get; set; }
+
+        public string? Phone { get; set; }
+
+        public string? ImageUrl { get; set; }
+
+        public string? WorkingHours { get; set; }
+
+        public string? Description { get; set; }
+
+        public string? Coordinates { get; set; }
+    }
+}
---
+++ src/Web/GarageBuddy.Web/Areas/Admin/Controllers/GarageController.cs
@@ -0,0 +1,27 @@
+﻿namespace GarageBuddy.Web.Areas.Admin.Controllers
+{
+    using Microsoft.AspNetCore.Mvc;
+    using Services.Data.Contracts;
+
+    public class GarageController : AdminController
+    {
+        IGarageService garageService;
+
+        public GarageController(IGarageService garageService)
+        {
+            this.garageService = garageService;
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+    }
+}
